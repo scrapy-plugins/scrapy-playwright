ecc24f048a20a4b43e6d06991aac6441342cec16
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2021-06-19 12:44:22 -0300

    Test dynamic context creation

--- tests/test_browser_contexts.py
+++ tests/test_browser_contexts.py
@@ -9,9 +9,7 @@ from scrapy_playwright.handler import ScrapyPlaywrightDownloadHandler
 from tests.mockserver import StaticMockServer
 
 
-class TestCaseMultipleContexts:
-    browser_type = "chromium"
-
+class MixinTestCaseMultipleContexts:
     @pytest.mark.asyncio
     async def test_contexts_startup(self):
         settings = {
@@ -52,11 +50,51 @@ class TestCaseMultipleContexts:
         await page.close()
         await handler.browser.close()
 
+    @pytest.mark.asyncio
+    async def test_contexts_dynamic(self):
+        settings = {"PLAYWRIGHT_BROWSER_TYPE": self.browser_type}
+        handler = ScrapyPlaywrightDownloadHandler(get_crawler(settings_dict=settings))
+        await handler._launch_browser()
 
-class TestCaseMultipleContextsFirefox(TestCaseMultipleContexts):
+        with StaticMockServer() as server:
+            meta = {
+                "playwright": True,
+                "playwright_include_page": True,
+                "playwright_context": "new",
+                "playwright_context_kwargs": {
+                    "storage_state": {
+                        "cookies": [
+                            {
+                                "url": "https://example.org",
+                                "name": "asdf",
+                                "value": "qwerty",
+                            },
+                        ],
+                    },
+                },
+            }
+            req = Request(server.urljoin("/index.html"), meta=meta)
+            resp = await handler._download_request(req, Spider("foo"))
+
+        page = resp.meta["playwright_page"]
+        storage_state = await page.context.storage_state()
+        cookie = storage_state["cookies"][0]
+        assert cookie["name"] == "asdf"
+        assert cookie["value"] == "qwerty"
+        assert cookie["domain"] == "example.org"
+
+        await page.close()
+        await handler.browser.close()
+
+
+class TestCaseMultipleContextsChromium(MixinTestCaseMultipleContexts):
+    browser_type = "chromium"
+
+
+class TestCaseMultipleContextsFirefox(MixinTestCaseMultipleContexts):
     browser_type = "firefox"
 
 
 @pytest.mark.skipif(platform.system() != "Darwin", reason="Test WebKit only on Darwin")
-class TestCaseMultipleContextsWebkit(TestCaseMultipleContexts):
+class TestCaseMultipleContextsWebkit(MixinTestCaseMultipleContexts):
     browser_type = "webkit"
