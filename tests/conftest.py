e573578c764eec6221c37251ea30b409c5a1e2ff
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2021-10-08 11:07:57 -0300

    Flake8 adjustment

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,8 @@
 Taken from
 https://github.com/pytest-dev/pytest-asyncio/blob/25cf2b399e00a82b69951474eed074ba26cd0c3b/pytest_asyncio/plugin.py
 
-Modify pytest_pycollect_makeitem to make use of the Function API in pytest>=5.4.0 (pytest.Function.from_parent).
+Modify pytest_pycollect_makeitem to make use of the Function API
+in pytest>=5.4.0 (pytest.Function.from_parent).
 
 In the context of scrapy-playwright, this allows to unpin the outdated pytest<5.4.0 dependency,
 while keeping pytest-asyncio==0.10, as pytest-asyncio>=0.11 currently breaks tests
