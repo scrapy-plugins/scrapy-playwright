67b446e63bbd82f79c1f898090077b812b25640f
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2021-10-08 17:30:45 -0300

    Remove testing no-op

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,14 +16,6 @@ import inspect
 
 import pytest
 
-try:
-    from _pytest.python import transfer_markers
-except ImportError:  # Pytest 4.1.0 removes the transfer_marker api (#104)
-
-    def transfer_markers(*args, **kwargs):  # noqa
-        """Noop when over pytest 4.1.0"""
-        pass
-
 
 def _is_coroutine(obj):
     """Check to see if an object is really an asyncio coroutine."""
@@ -35,12 +27,5 @@ def pytest_pycollect_makeitem(collector, name, obj):
     """A pytest hook to collect asyncio coroutines."""
     if collector.funcnamefilter(name) and _is_coroutine(obj):
         item = pytest.Function.from_parent(collector, name=name)
-
-        # Due to how pytest test collection works, module-level pytestmarks
-        # are applied after the collection step. Since this is the collection
-        # step, we look ourselves.
-        transfer_markers(obj, item.cls, item.module)
-        item = pytest.Function.from_parent(collector, name=name)  # To reload keywords.
-
         if "asyncio" in item.keywords:
             return list(collector._genfunctions(name, obj))
