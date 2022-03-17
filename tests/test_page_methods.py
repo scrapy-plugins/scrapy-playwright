31dd51a256e1f29b7141d0ba325f3d4f0d78f9c2
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2022-03-17 01:41:19 -0300

    Test deprecation messages

--- tests/test_page_methods.py
+++ tests/test_page_methods.py
@@ -1,11 +1,12 @@
 import logging
 import platform
+import warnings
 
 import pytest
 from scrapy import Spider, Request
 from scrapy.http.response.html import HtmlResponse
 
-from scrapy_playwright.page import PageMethod
+from scrapy_playwright.page import PageMethod, PageCoroutine
 
 from tests import make_handler
 from tests.mockserver import StaticMockServer
@@ -21,7 +22,24 @@ async def test_page_methods():
     assert str(screenshot) == "<PageMethod for method 'screenshot'>"
 
 
+@pytest.mark.asyncio
+async def test_deprecated_class():
+    with warnings.catch_warnings(record=True) as warning_list:
+        PageCoroutine("screenshot", "foo", 123, path="/tmp/file", type="png")
+        assert str(warning_list[0].message) == (
+            "The scrapy_playwright.page.PageCoroutine class is deprecated, "
+            "please use scrapy_playwright.page.PageMethod instead."
+        )
+
+
 class MixinPageMethodTestCase:
+    def assert_correct_response(self, response: HtmlResponse, request: Request) -> None:
+        assert isinstance(response, HtmlResponse)
+        assert response.request is request
+        assert response.url == request.url
+        assert response.status == 200
+        assert "playwright" in response.flags
+
     @pytest.mark.asyncio
     async def test_page_non_page_method(self, caplog):
         async with make_handler({"PLAYWRIGHT_BROWSER_TYPE": self.browser_type}) as handler:
@@ -39,18 +57,13 @@ class MixinPageMethodTestCase:
                 )
                 resp = await handler._download_request(req, Spider("foo"))
 
-            assert isinstance(resp, HtmlResponse)
-            assert resp.request is req
-            assert resp.url == server.urljoin("/index.html")
-            assert resp.status == 200
-            assert "playwright" in resp.flags
-
-            for obj in req.meta["playwright_page_methods"]:
-                assert (
-                    "scrapy-playwright",
-                    logging.WARNING,
-                    f"Ignoring {repr(obj)}: expected PageMethod, got {repr(type(obj))}",
-                ) in caplog.record_tuples
+        self.assert_correct_response(resp, req)
+        for obj in req.meta["playwright_page_methods"]:
+            assert (
+                "scrapy-playwright",
+                logging.WARNING,
+                f"Ignoring {repr(obj)}: expected PageMethod, got {repr(type(obj))}",
+            ) in caplog.record_tuples
 
     @pytest.mark.asyncio
     async def test_page_mixed_page_methods(self, caplog):
@@ -69,20 +82,37 @@ class MixinPageMethodTestCase:
                 )
                 resp = await handler._download_request(req, Spider("foo"))
 
-            assert isinstance(resp, HtmlResponse)
-            assert resp.request is req
-            assert resp.url == server.urljoin("/index.html")
-            assert resp.status == 200
-            assert "playwright" in resp.flags
+        self.assert_correct_response(resp, req)
+        does_not_exist = req.meta["playwright_page_methods"]["does_not_exist"]
+        assert (
+            "scrapy-playwright",
+            logging.WARNING,
+            f"Ignoring {repr(does_not_exist)}: could not find method",
+        ) in caplog.record_tuples
+        assert not req.meta["playwright_page_methods"]["is_closed"].result
+        assert req.meta["playwright_page_methods"]["title"].result == "Awesome site"
 
-            does_not_exist = req.meta["playwright_page_methods"]["does_not_exist"]
-            assert (
-                "scrapy-playwright",
-                logging.WARNING,
-                f"Ignoring {repr(does_not_exist)}: could not find method",
-            ) in caplog.record_tuples
-            assert not req.meta["playwright_page_methods"]["is_closed"].result
-            assert req.meta["playwright_page_methods"]["title"].result == "Awesome site"
+    @pytest.mark.asyncio
+    async def test_deprecated(self, caplog):
+        async with make_handler({"PLAYWRIGHT_BROWSER_TYPE": self.browser_type}) as handler:
+            with StaticMockServer() as server:
+                req = Request(
+                    url=server.urljoin("/index.html"),
+                    meta={
+                        "playwright": True,
+                        "playwright_page_coroutines": [
+                            PageMethod("is_closed"),
+                        ],
+                    },
+                )
+                with warnings.catch_warnings(record=True) as warning_list:
+                    resp = await handler._download_request(req, Spider("foo"))
+
+        self.assert_correct_response(resp, req)
+        assert str(warning_list[0].message) == (
+            "The 'playwright_page_coroutines' request meta key is deprecated,"
+            " please use 'playwright_page_methods' instead."
+        )
 
 
 class TestPageMethodChromium(MixinPageMethodTestCase):
