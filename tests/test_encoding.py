63228164e0701b5ea8340af797613fb9c52f3a8e
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2022-03-22 22:08:27 -0300

    Test docstrings

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -18,6 +18,7 @@ def body_str(charset: str, content: str = "áéíóú") -> str:
 
 @pytest.mark.asyncio
 async def test_encode_from_headers():
+    """Charset declared in headers takes precedence"""
     text = body_str("gb2312")
     body, encoding = _encode_body(
         headers=Headers({"content-type": "text/html; charset=ISO-8859-1"}),
@@ -29,22 +30,27 @@ async def test_encode_from_headers():
 
 @pytest.mark.asyncio
 async def test_encode_from_body():
+    """No charset declared in headers, use the one declared in the body"""
     text = body_str("gb2312")
     body, encoding = _encode_body(headers=Headers({}), text=text)
     assert encoding == "gb18030"
-    assert body == body_str("gb2312").encode(encoding)
+    assert body == text.encode(encoding)
 
 
 @pytest.mark.asyncio
 async def test_encode_fallback():
+    """No charset declared, use utf-8 as fallback"""
     text = "<html>áéíóú</html>"
     body, encoding = _encode_body(headers=Headers(), text=text)
     assert encoding == "utf-8"
-    assert body == "<html>áéíóú</html>".encode(encoding)
+    assert body == text.encode(encoding)
 
 
 @pytest.mark.asyncio
 async def test_encode_mismatch():
+    """Charset declared in headers and body do not match, and the headers
+    one fails to encode: use the one in the body (first one that works)
+    """
     text = body_str("gb2312", content="空手道")
     body, encoding = _encode_body(
         headers=Headers({"content-type": "text/html; charset=ISO-8859-1"}),
