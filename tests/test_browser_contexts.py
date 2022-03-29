af1bfe06c6f68270f029b4451d78cb39df6fa6c3
elacuesta <1731933+elacuesta@users.noreply.github.com>
2022-03-29 10:47:23 -0300

    Remove deprecated PLAYWRIGHT_CONTEXT_ARGS setting (#75)
    * Remove deprecated PLAYWRIGHT_CONTEXT_ARGS setting
    
    * Update changelog

--- tests/test_browser_contexts.py
+++ tests/test_browser_contexts.py
@@ -1,10 +1,8 @@
 import asyncio
 import platform
-import warnings
 
 import pytest
 from scrapy import Spider, Request
-from scrapy.exceptions import ScrapyDeprecationWarning
 
 from tests import make_handler
 from tests.mockserver import StaticMockServer
@@ -135,49 +133,6 @@ class MixinTestCaseMultipleContexts:
             assert cookie["name"] == "asdf"
             assert cookie["value"] == "qwerty"
             assert cookie["domain"] == "example.org"
-
-    @pytest.mark.asyncio
-    async def test_deprecated_setting(self):
-        settings = {
-            "PLAYWRIGHT_BROWSER_TYPE": self.browser_type,
-            "PLAYWRIGHT_CONTEXT_ARGS": {
-                "storage_state": {
-                    "cookies": [
-                        {
-                            "url": "https://example.org",
-                            "name": "asdf",
-                            "value": "qwerty",
-                        },
-                    ],
-                },
-            },
-        }
-        with warnings.catch_warnings(record=True) as warning_list:
-            async with make_handler(settings) as handler:
-                assert warning_list[0].category is ScrapyDeprecationWarning
-                assert str(warning_list[0].message) == (
-                    "The PLAYWRIGHT_CONTEXT_ARGS setting is deprecated, please use"
-                    " PLAYWRIGHT_CONTEXTS instead. Keyword arguments defined in"
-                    " PLAYWRIGHT_CONTEXT_ARGS will be used when creating the 'default' context"
-                )
-                assert len(handler.contexts) == 1
-                assert len(handler.context_semaphores) == 1
-
-                with StaticMockServer() as server:
-                    meta = {
-                        "playwright": True,
-                        "playwright_include_page": True,
-                    }
-                    req = Request(server.urljoin("/index.html"), meta=meta)
-                    resp = await handler._download_request(req, Spider("foo"))
-
-                page = resp.meta["playwright_page"]
-                storage_state = await page.context.storage_state()
-                await page.close()
-                cookie = storage_state["cookies"][0]
-                assert cookie["name"] == "asdf"
-                assert cookie["value"] == "qwerty"
-                assert cookie["domain"] == "example.org"
 
 
 class TestCaseMultipleContextsChromium(MixinTestCaseMultipleContexts):
