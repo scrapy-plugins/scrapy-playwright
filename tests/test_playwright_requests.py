a54b0fdf26d8f328fcfe87b522bd333e73671226
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2022-05-05 21:21:10 -0300

    Simplify tests

--- tests/test_playwright_requests.py
+++ tests/test_playwright_requests.py
@@ -433,50 +433,36 @@ class MixinTestCase:
 
     @pytest.mark.asyncio
     async def test_abort_requests(self):
-        settings_dict = {
-            "PLAYWRIGHT_BROWSER_TYPE": self.browser_type,
-            "PLAYWRIGHT_ABORT_REQUEST": lambda req: req.resource_type == "image",
-        }
-        async with make_handler(settings_dict) as handler:
-            with StaticMockServer() as server:
-                req = Request(
-                    url=server.urljoin("/gallery.html"),
-                    meta={"playwright": True},
-                )
-                await handler._download_request(req, Spider("foo"))
-
-                req_prefix = "playwright/request_count"
-                resp_prefix = "playwright/response_count"
-                assert handler.stats.get_value(f"{req_prefix}/resource_type/document") == 1
-                assert handler.stats.get_value(f"{req_prefix}/resource_type/image") == 3
-                assert handler.stats.get_value(f"{resp_prefix}/resource_type/document") == 1
-                assert handler.stats.get_value(f"{resp_prefix}/resource_type/image") is None
-                assert handler.stats.get_value(f"{req_prefix}/aborted") == 3
-
-    @pytest.mark.asyncio
-    async def test_abort_requests_coroutine(self):
-        async def should_abort_request(request):
+        async def should_abort_request_async(request):
             return request.resource_type == "image"
 
-        settings_dict = {
-            "PLAYWRIGHT_BROWSER_TYPE": self.browser_type,
-            "PLAYWRIGHT_ABORT_REQUEST": should_abort_request,
-        }
-        async with make_handler(settings_dict) as handler:
-            with StaticMockServer() as server:
-                req = Request(
-                    url=server.urljoin("/gallery.html"),
-                    meta={"playwright": True},
-                )
-                await handler._download_request(req, Spider("foo"))
+        def should_abort_request_sync(request):
+            return request.resource_type == "image"
 
-                req_prefix = "playwright/request_count"
-                resp_prefix = "playwright/response_count"
-                assert handler.stats.get_value(f"{req_prefix}/resource_type/document") == 1
-                assert handler.stats.get_value(f"{req_prefix}/resource_type/image") == 3
-                assert handler.stats.get_value(f"{resp_prefix}/resource_type/document") == 1
-                assert handler.stats.get_value(f"{resp_prefix}/resource_type/image") is None
-                assert handler.stats.get_value(f"{req_prefix}/aborted") == 3
+        for predicate in (
+            lambda request: request.resource_type == "image",
+            should_abort_request_async,
+            should_abort_request_sync,
+        ):
+            settings_dict = {
+                "PLAYWRIGHT_BROWSER_TYPE": self.browser_type,
+                "PLAYWRIGHT_ABORT_REQUEST": predicate,
+            }
+            async with make_handler(settings_dict) as handler:
+                with StaticMockServer() as server:
+                    req = Request(
+                        url=server.urljoin("/gallery.html"),
+                        meta={"playwright": True},
+                    )
+                    await handler._download_request(req, Spider("foo"))
+
+                    req_prefix = "playwright/request_count"
+                    resp_prefix = "playwright/response_count"
+                    assert handler.stats.get_value(f"{req_prefix}/resource_type/document") == 1
+                    assert handler.stats.get_value(f"{req_prefix}/resource_type/image") == 3
+                    assert handler.stats.get_value(f"{resp_prefix}/resource_type/document") == 1
+                    assert handler.stats.get_value(f"{resp_prefix}/resource_type/image") is None
+                    assert handler.stats.get_value(f"{req_prefix}/aborted") == 3
 
 
 class TestCaseChromium(MixinTestCase):
