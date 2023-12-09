19d635b8665f23dee27238aaf99b2997124c6c0f
Eugenio Lacuesta <1731933+elacuesta@users.noreply.github.com>
2023-12-09 16:31:12 -0300

    Test on Python 3.12 (#254)

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Framework :: Scrapy",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP",
