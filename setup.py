d130d2bf889342600c616e14109940f82f333393
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2026-01-16 16:18:09 -0300

    Remove py314 for now

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
         "Framework :: Scrapy",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP",
