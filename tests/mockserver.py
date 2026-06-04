2a9fa0c15660b019f036977c86c8317c65652a87
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2026-06-04 12:26:57 -0300

    Fix elif in tests

--- tests/mockserver.py
+++ tests/mockserver.py
@@ -59,7 +59,7 @@ class _RequestHandler(BaseHTTPRequestHandler):
 
         if parsed_path.path == "/headers":
             self._send_json(dict(self.headers))
-        if parsed_path.path == "/asdf":
+        elif parsed_path.path == "/asdf":
             self._send_json({"asdf": "asdf"})
         elif parsed_path.path == "/status/204":
             self.send_response(204)
